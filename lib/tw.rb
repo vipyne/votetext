f229bc1f323bbecb63b00e77d4a952db8d4963a9
vanessa <vipyne@gmail.com>
2014-01-16 23:33:53 -0500

    when env variable hardcoded, sms works!  working to resolve env issue...

--- lib/tw.rb
+++ lib/tw.rb
@@ -1,11 +1,19 @@
 module Tw
 
+
   def make_client
+    p 'got to make_client'
+    p ENV["ACCOUNT_SID"]
+    p ENV["AUTH_TOKEN"]
     Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
   end
 
   def send_message to, message
-    client = Tw.make_client
+    p 'got to send_message'
+    client = self.make_client
+    p '&&&&&&&&&'
+    p client
+    p '&&&&&&&&&'
     client.account.messages.create({
       :from => '+14846794637',
       :to => to,
