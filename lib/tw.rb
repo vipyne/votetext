de8bb73a01077a3b685b5c481a603825a13094f2
vanessa <vipyne@gmail.com>
2014-01-23 17:54:02 -0500

    added chronic gem; adjusted button css

--- lib/tw.rb
+++ lib/tw.rb
@@ -4,12 +4,19 @@ module Tw
     Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"]
   end
 
-  def send_message message
+  def send_message show_hash
+    now = Time.now
+    message = []
+    show_hash.each do |date, election|
+      elec_date = date
+      the_when = Chronic.parse("#{elec_date}")
+      message << now - the_when
+    end
     client = self.make_twilio_client
     client.account.messages.create({
       :from => "+14846794637",
       :to => "+#{current_user.phonenumber}",
-      :body => message # :message
+      :body => message.to_s # :message
     })
   end
 
