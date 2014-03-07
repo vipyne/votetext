ef16acb8ab361e5059cc2f3ff2fcc1fe54e9a675
vanessa <vipyne@gmail.com>
2014-03-07 15:18:40 -0500

    removed unneeded line... commented update route that'll probably be going away

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,10 @@ class UsersController < ApplicationController
     @user = current_user
     state = current_user.state
     city = current_user.city
-    ids = get_elections state, city
     if current_user.submit == "get representatives"
       if get_reps current_user.full_address
         @reps = get_reps current_user.full_address
+        @user.update_attributes :submit => "get representatives"
       else
         @reps = "sorry, that address didn't work. maybe there was a typo?"
       end
@@ -41,6 +41,19 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  # def update
+  #   @user = current_user
+  #   state = current_user.state
+  #   city = current_user.city
+  #   if get_candidates state, city
+  #     @show = get_candidates state, city
+  #     send_message @show
+  #   else
+  #     @show = "sorry, that address didn't work. maybe there was a typo?"
+  #   end
+  #   redirect_to user_path @user
+  # end
 
   helper_method :current_user
   helper_method :login
